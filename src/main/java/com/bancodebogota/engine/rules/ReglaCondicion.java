67d2c8b02218389be3bbc64c46b21d202c9c4436
mesa963 <mesa963@gmail.com>
2026-02-22 18:38:53 -0500

    Update ReglaCondicion.java

--- src/main/java/com/bancodebogota/engine/rules/ReglaCondicion.java
+++ src/main/java/com/bancodebogota/engine/rules/ReglaCondicion.java
@@ -20,9 +20,18 @@ public class ReglaCondicion implements ReglaMigracion {
             String indent = matcher.group(1);
             String condition = matcher.group(2).trim();
 
-            condition = condition.replace("'", "\"");
+            Pattern stringEqPattern = Pattern.compile("([a-zA-Z_0-9]+)\\s*=\\s*('[^']+')");
+            Matcher m = stringEqPattern.matcher(condition);
+            if (m.find()) {
+                condition = m.replaceAll(matchResult -> matchResult.group(1).toLowerCase() + ".equals("
+                        + matchResult.group(2).replace("'", "\"") + ")");
+            } else {
 
-            return indent + "if (" + condition.toLowerCase() + ") {";
+                condition = condition.replace("=", "==");
+                condition = condition.toLowerCase();
+            }
+
+            return indent + "if (" + condition + ") {";
         }
         return linea;
     }
